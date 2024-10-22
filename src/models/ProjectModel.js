d258931a44ad6f93e3d9fc3bc7f1cd35b003ff8a
Andrea G <jaanguillende@ittepic.edu.mx>
2024-10-22 00:19:15 -0700

    Commit

---
+++ src/models/ProjectModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const projectSchema =new mongoose.Schema({
+id: {type: String, require: true},
+title: {type : String, require :true},
+fechaIni: {type: String, require: true},
+fechaFin: {type: String, require: true},
+status: {type: String, default: 'Planificado'},
+presupuesto: {type: Number, require: true}
+});
+
+const Project = mongoose.model('Project', projectSchema);
+module.exports = Project;
